9255d5eccf9ed08c704816af995448ca286b5631
Jacob <jacobshaw5668@gmail.com>
2020-06-03 11:36:25 -0400

    Add Delete Button

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,55 +1,53 @@
-import React, {Component} from "react"
-import Todo from "../services/Todo";
-import {getTodos} from "../services/todoService";
-import TodoItem from "./TodoItem";
-import {Space, Row} from "antd";
-import TodoForm from "./TodoForm";
-import {inspect} from "util";
-import styles from "./styles.module.css"
+import React from 'react';
+import { Input, Button, Form, Switch } from 'antd';
+import styles from './styles.module.css';
+import {postTodo} from "../services/todoService";
 
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 8 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 8 },
+};
 
-
-
-interface TodoListState {
-    todos: Todo[];
-    loading: boolean;
+interface TodoFormInterface {
+    reload(): void;
 }
 
-class TodoList extends Component<any, TodoListState> {
-    state = {
-        todos: [],
-        loading: true
-    }
+const TodoForm = (props: TodoFormInterface) => {
+    const [form] = Form.useForm();
 
-    async componentDidMount(){
-        await this.loadPage();
+    const onFinish = (values: any) => {
+        console.log(values);
+        postTodo(values).then(() => props.reload());
+    };
 
-    }
+    const onReset = () => {
+        form.resetFields();
+    };
 
-    loadPage = async() => {
-        let todos = await getTodos();
-        this.setState({todos, loading: false});
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>This is a todo list</h2>
-                {this.state.loading ?(
-                  <h2>this is loading</h2>
-                ) : (
-                    <>
-                    <TodoForm reload={this.loadPage}/>
-                        <Row className={styles.todoRows}>
-                    {this.state.todos.map((todo: Todo) =>
-                        <TodoItem key={todo.id} todo={todo} />
-                        )}
-                    </Row>
-                    </>
-                )}
-            </div>
-        )
-    }
+    return (
+        <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+            <Form.Item name="title" label="Title" rules={[{ required: true}]}>
+                <Input />
+            </Form.Item>
+            <Form.Item name="description" label="Description" rules={[{ required: true}]}>
+                <Input.TextArea />
+            </Form.Item>
+            <Form.Item name="complete" label="Complete" valuePropName="checked" initialValue={false}>
+                <Switch />
+            </Form.Item>
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+                <Button htmlType="button" onClick={onReset} className={styles.button}>
+                    Reset
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 }
 
-export default TodoList;
+export default TodoForm;
